6e2fa78e5c7b151c30a18956c5eedb66f56712ee
Kit Jirles <kjirles13@gmail.com>
2023-05-07 15:37:27 -0400

    Add bio fixture and add profile components to meet-the-team

---
+++ src/fixtures/meet-the-team.tsx
@@ -0,0 +1,22 @@
+import headshot1 from '../public/headshot.jpg';
+
+
+const bios = [
+    {
+        name: "Erin",
+        headshot: headshot1.src,
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec sapien mi. Nulla lacus lorem, aliquam ut lorem eu, eleifend efficitur erat. Maecenas porttitor sem eu tristique pretium. Quisque nec purus ornare, hendrerit odio sed, sollicitudin nunc. Donec sodales odio sit amet lectus sagittis pretium. Integer vitae justo nec ex accumsan tincidunt. Nulla eu urna eget lorem vehicula efficitur et ut orci. Morbi ligula leo, gravida ut hendrerit eu, blandit eget lacus. Nulla ligula tortor, ultrices non tristique nec, interdum a diam. Fusce volutpat ipsum non turpis porta varius. Curabitur laoreet nisi efficitur risus bibendum rutrum. Quisque sollicitudin mattis tortor, vitae ultrices felis dictum nec.\n\nSed sem nunc, tincidunt vel ornare ut, malesuada eu urna. Sed id elementum turpis. Ut dapibus ante augue, sit amet pellentesque erat vehicula sed. Etiam elementum efficitur aliquet. Morbi ac aliquam quam, vel viverra sem. Phasellus dictum tortor sed semper porta. Fusce ex ante, aliquet at varius non, dapibus ut massa. Quisque lacus dolor, volutpat sit amet dui ac, facilisis gravida elit. Ut diam sem, varius sed placerat et, lacinia eu enim. Nam lorem felis, varius at nulla non, tempor rutrum risus. Pellentesque sapien ligula, tempor quis egestas ut, porttitor non quam. Aenean convallis tellus id lectus vehicula, vitae viverra risus gravida. Integer condimentum justo felis, tristique sollicitudin dolor faucibus eget. Pellentesque lacinia tempus imperdiet."
+    },
+    {
+        name: "Cassidy",
+        headshot: headshot1.src,
+        bio: "Proin tincidunt luctus rutrum. Fusce dui lorem, convallis ac ante ut, vehicula imperdiet nibh. Integer non turpis mattis, tempus tortor vitae, tincidunt ante. Cras pellentesque diam quis dui mattis convallis. Donec nec dapibus massa, imperdiet venenatis ipsum. Nullam elit purus, ullamcorper quis elementum ut, scelerisque eget velit. Ut vel hendrerit nunc. Aliquam nec elementum nulla, a pharetra libero. Suspendisse potenti. Maecenas id lorem eget mauris accumsan auctor. Pellentesque id sem ex. Phasellus elit justo, dignissim eu neque quis, imperdiet porttitor sapien. Quisque tempus orci pretium dolor porta consectetur. Fusce nec velit aliquam, commodo enim id, porta enim.\n\nMaecenas vel lorem leo. Pellentesque porta facilisis vulputate. In hac habitasse platea dictumst. Nulla facilisi. Pellentesque consequat molestie mauris. Integer non odio ut sapien finibus aliquam at eu sem. Sed diam ex, aliquet nec nisi vel, varius aliquam justo. Pellentesque sit amet magna sit amet libero venenatis elementum sed ac tellus. Maecenas magna tortor, eleifend ut commodo ut, posuere eget tellus. Aliquam erat volutpat. Nullam cursus, magna eget aliquam facilisis, eros enim mattis sapien, in lobortis massa neque lacinia nulla. Nullam sed facilisis elit. Sed iaculis suscipit suscipit."
+    },
+    {
+        name: "Morgan",
+        headshot: headshot1.src,
+        bio: "Proin tincidunt luctus rutrum. Fusce dui lorem, convallis ac ante ut, vehicula imperdiet nibh. Integer non turpis mattis, tempus tortor vitae, tincidunt ante. Cras pellentesque diam quis dui mattis convallis. Donec nec dapibus massa, imperdiet venenatis ipsum. Nullam elit purus, ullamcorper quis elementum ut, scelerisque eget velit. Ut vel hendrerit nunc. Aliquam nec elementum nulla, a pharetra libero. Suspendisse potenti. Maecenas id lorem eget mauris accumsan auctor. Pellentesque id sem ex. Phasellus elit justo, dignissim eu neque quis, imperdiet porttitor sapien. Quisque tempus orci pretium dolor porta consectetur. Fusce nec velit aliquam, commodo enim id, porta enim.\n\nMaecenas vel lorem leo. Pellentesque porta facilisis vulputate. In hac habitasse platea dictumst. Nulla facilisi. Pellentesque consequat molestie mauris. Integer non odio ut sapien finibus aliquam at eu sem. Sed diam ex, aliquet nec nisi vel, varius aliquam justo. Pellentesque sit amet magna sit amet libero venenatis elementum sed ac tellus. Maecenas magna tortor, eleifend ut commodo ut, posuere eget tellus. Aliquam erat volutpat. Nullam cursus, magna eget aliquam facilisis, eros enim mattis sapien, in lobortis massa neque lacinia nulla. Nullam sed facilisis elit. Sed iaculis suscipit suscipit."
+    }
+];
+
+export default bios;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,15 @@
-import Navbar from '../components/navbar';
 import Profile from '../components/profile';
+import bios from '../fixtures/meet-the-team';
 
 export default function About() {
     return (
         <>
             <div>
-                <h1>Meet the Team</h1>
-                <Profile/>
+                <h1 style={{textAlign: 'center'}}>Who We Are</h1>
+                <br />
+                {bios.map(bio => (
+                    <Profile headshot={bio.headshot} bio={bio.bio} name={bio.name} key={bio.name}/>
+                ))}
             </div>
 
         </>
