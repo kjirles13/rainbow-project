68f734a36b2898d53d9d6b6a1417f936ee9de2c4
Kit Jirles <kjirles13@gmail.com>
2023-05-07 13:36:17 -0400

    Add icons and links to Facebook and Instagram in Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,23 @@
-import { colors } from "@mui/material"
-import { blue } from "@mui/material/colors"
+import Link from 'next/link';
+import facebook from '../public/facebook.png';
+import instagram from '../public/instagram.png';
+import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
     return (
-        <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', background: 'rgb(213, 215, 236)'}}>
-        <p> 1234 Rainbow Rd <br />
-            Morgantown, WV 25508 <br />
-        </p>
-        <p> E: projectrainbowwv@gmail.com</p>
+        <div className={styles.wrapper}>
+            <div className={styles.icons}>
+                <a href="https://www.facebook.com/people/Project-Rainbow/100088104047306/" target="_blank" >
+                    <img src={facebook.src} alt="Facebook"/>
+                </a>
+                <a href="https://www.instagram.com/project_rainbow_wv/" target="_blank">
+                    <img src={instagram.src} alt="Instagram" />
+                </a>
+            </div>
+            <p> E: projectrainbowwv@gmail.com</p>
+            <p> 1234 Rainbow Rd <br />
+                Morgantown, WV 25508 <br />
+            </p>
         </div>
     )
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,18 +5,20 @@ import styles from "../styles/Layout.module.css";
 
 export default function Layout({ children }: any) {
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.header}>
-            <Navbar />
-            </div>
-            <main className={styles.main}>
-            {children}
-            </main>
-            <div className={styles.sidebar}>
-            <Sidebar></Sidebar>
-            </div>
-            <div className={styles.footer}>
-            <Footer />
+        <div className={styles.body}>
+            <div className={styles.wrapper}>
+                <div className={styles.header}>
+                    <Navbar />
+                </div>
+                <main className={styles.main}>
+                    {children}
+                </main>
+                <div className={styles.sidebar}>
+                    <Sidebar></Sidebar>
+                </div>
+                <div className={styles.footer}>
+                    <Footer />
+                </div>
             </div>
         </div>
     )
