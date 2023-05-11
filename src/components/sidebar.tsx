4521f0b09bf163c7fcc0bab9e912dffde291e33e
Kit Jirles <kjirles13@gmail.com>
2023-05-11 12:48:48 -0400

    Modify design of sidebar stats and media sizes for navbar and home page

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,21 +3,23 @@ import styles from '../styles/Sidebar.module.css';
 export default function Sidebar() {
     return (
         <>
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+                <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&family=Raleway:wght@300&display=swap" rel="stylesheet" />
+            </head>
             <h1 className={styles["stats-title"]}>The Stats</h1>
             <div className={styles.container}>
-                <head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-                    <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&family=Raleway:wght@300&display=swap" rel="stylesheet" />
-                </head>
+                <div className={styles["stats-container"]}>
                 <ul className={styles["stats-list"]}>
-                    <li>LGBTQ+ youth are <strong>120% </strong>more likely to experience homelessness in their lifetime</li>
-                    <li><strong>40% </strong> of the youth experiencing homelessness identify as LGBTQ+, despite making up <strong>10% </strong>of the population</li>
-                    <li><strong>38%</strong> of unsheltered LGBTQ+ youth reported experiencing sexual assault</li>
-                    <li><strong>62%</strong> of LGBTQ+ youth experiencing homelessness have reported physical assault</li>
-                    <li>LGBTQ+ people are <strong>4 times </strong> more likely than straight, cisgender people to experience violence based on their sexuality or gender identity</li>
-                    <li><strong>43%</strong> of unsheltered LGBTQ+ youth report becoming unhoused due to rejection of their sexuality or gender identity by their family</li>
+                    <li>LGBTQ+ youth are <span className={styles["highlight-container"]}><span className={styles.highlight}>120% </span></span> more likely to experience homelessness in their lifetime</li>
+                    <li><span className={styles["highlight-container"]}><span className={styles.highlight}>40% </span></span> of the youth experiencing homelessness identify as LGBTQ+, despite making up <span className={styles["highlight-container"]}><span className={styles.highlight}>10%</span></span> of the population</li>
+                    <li><span className={styles["highlight-container"]}><span className={styles.highlight}>38% </span></span> of unsheltered LGBTQ+ youth reported experiencing sexual assault</li>
+                    <li><span className={styles["highlight-container"]}><span className={styles.highlight}>62% </span></span> of LGBTQ+ youth experiencing homelessness have reported physical assault</li>
+                    <li>LGBTQ+ people are <span className={styles["highlight-container"]}><span className={styles.highlight}>4 times</span></span> more likely than straight, cisgender people to experience violence based on their sexuality or gender identity</li>
+                    <li><span className={styles["highlight-container"]}><span className={styles.highlight}>43% </span></span> of unsheltered LGBTQ+ youth report becoming unhoused due to rejection of their sexuality or gender identity by their family</li>
                 </ul>
+                </div>
             </div>
         </>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import Sidebar from '../components/sidebar';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles["grid-body"]}>
-        <p className={styles.bio}>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+      <div className={styles["main"]}>
+        <p className={styles.profile}>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
           doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis
           et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
           voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
